67488bb6468682fd8885fb262b17ab7cdc302af9
TuanDe2k3 <tuancoder2k3@gmail.com>
2023-07-05 21:53:08 +0700

    update all

--- jscrip.js
+++ jscrip.js
@@ -2,6 +2,5 @@ let menu = document.querySelector('#menu-icon');
 let navlist = document.querySelector('.navlist');
 
 menu.onclick = () => {
-    menu.classList.toggle('bx-menu');
     navlist.classList.toggle('open');
 }
